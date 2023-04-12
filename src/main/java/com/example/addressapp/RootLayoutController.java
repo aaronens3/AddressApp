3868c46a211d4953660ec70afe364d737b622cf5
syncrolattex <diseno@syncrolattex.com>
2023-04-12 15:00:46 +0200

    Initial commit

---
+++ src/main/java/com/example/addressapp/RootLayoutController.java
@@ -0,0 +1,2 @@
+package com.example.addressapp;public class RootLayoutController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.addressapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.addressapp to javafx.fxml;
+    exports com.example.addressapp;
+}
